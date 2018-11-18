de06b33300328675582a3a2d6e1e03f7ff176241
Deven Bansod <devenbansod.bits@gmail.com>
2018-11-18 10:49:33 +0530

    v1.0.0

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pcap_to_ditg',
-      version='0.1.1',
+      version='1.0.0',
       description='Generate DITG script files from a pcap file',
       url='http://github.com/devenbansod/pcap_to_DITG',
       author='Deven Bansod',
       author_email='devenbansod.bits@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       license='LICENSE',
       packages=['pcap_to_ditg'],
       install_requires=[
           'dpkt',
       ],
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ])
